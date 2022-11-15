8d4fde4b468a30e66da49591d1470fb396f95606
ggiraldo95 <gisell.giraldo@gmail.com>
2022-11-14 21:56:52 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$("#about").click(
+  function myFunction() {
+     const element = document.getElementById("herolmg");
+     element.scrollIntoView();
+   })
+
+$("#skills").click(
+  function myFunction() {
+      const element = document.getElementById("myskills");
+      element.scrollIntoView();
+    })
+
+$("#work").click(
+  function myFunction() {
+      const element = document.getElementById("mywork");
+      element.scrollIntoView();
+    })
+    
+     
+  
